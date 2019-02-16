fa53a59e578691b01705043c8dd00328b6ebee2f
Alexander Foss <alexanderhfoss@gmail.com>
2019-02-16 16:48:43 -0700

    Added main routine

--- breaker.py
+++ breaker.py
@@ -1,10 +1,36 @@
 import itertools as it
+import sys
+
+# TODO: Allow guesses not in set of possibilities
+# TODO: Speedup main routine: use integer coding? cython?
+
+NUM_SPOTS = 3
 
 COLOR_LIST = [ 'black', 'blue', 'green', 'orange', 'pink', 'purple', 'white', 'yellow', ]
 COLOR_ABBREVS = ['a','u','g','o','i','r','w','y']
 COLOR_DICT = dict(zip(COLOR_ABBREVS, COLOR_LIST))
 
-ALL_GUESSES = list(it.product(COLOR_LIST, COLOR_LIST, COLOR_LIST, COLOR_LIST, COLOR_LIST))
+ALL_GUESSES = list(it.product(*[COLOR_ABBREVS]*NUM_SPOTS))
+
+def printProgressBar (iteration, total, prefix = 'Calculating next guess:', suffix = 'Complete', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
 def expand_color_string(ss):
     as_list = [s for s in ss]
@@ -12,12 +38,12 @@ def expand_color_string(ss):
 
 class ColorCounter:
     def __init__(self, guess):
-        self.color_counts = dict(zip(COLOR_LIST, [0]*8))
+        self.color_counts = dict(zip(COLOR_ABBREVS, [0]*8))
         for elm in guess:
             self.color_counts[elm] += 1
     def countMatches(self, ccounter):
         counter = 0
-        for color in COLOR_LIST:
+        for color in COLOR_ABBREVS:
             num_guess = self.color_counts[color]
             num_truth = ccounter.color_counts[color]
             counter += min(num_guess,num_truth)
@@ -34,7 +60,7 @@ def score_guess(guess, truth):
 
 def valid_possibility(possibility, history):
     """
-    possibility: a color 5-tuple
+    possibility: a color 5-tuple (adjust 5 based on NUM_SPOTS)
     history: a list of (color 5-tuple, (r,w)) tuples
     """
     return all([hh[1] == score_guess(guess = hh[0], truth = possibility) for hh in history])
@@ -47,31 +73,54 @@ def update_possibilities(possibilities, history):
     return [pp for pp in possibilities if valid_possibility(pp, history)]
 
 if __name__ == "__main__":
-#    print("expand_color_string('uairwy')")
-#    print(expand_color_string('uairwy'))
-    c1 = ColorCounter(expand_color_string('yauga'))
-    c2 = ColorCounter(expand_color_string('aarrr'))
-#    print("c1.color_counts")
-#    print(c1.color_counts)
-#    print("c1.countMatches(c2)")
-#    print(c1.countMatches(c2))
-    print("score_guess(expand_color_string('yauga'),expand_color_string('yauar'))")
-    print(score_guess(expand_color_string('yauga'),expand_color_string('yauar')))
-    sample_poss = [ALL_GUESSES[i] for i in [5,60,600,500]]
-    print("sample_poss")
-    print(sample_poss)
-    sample_history = [
-        (('black', 'black', 'black', 'black', 'black'),(2,0)),
-        (('pink', 'pink', 'pink', 'pink', 'pink'),(0,0)),
-    ]
-    print("sample_history")
-    print(sample_history)
-    print("valid_possibility(sample_poss[0], sample_history)")
-    print(valid_possibility(sample_poss[0], sample_history))
-    print("update_possibilities(sample_poss, sample_history)")
-    print(update_possibilities(sample_poss, sample_history))
+#    c1 = ColorCounter(expand_color_string('yauga'))
+#    c2 = ColorCounter(expand_color_string('aarrr'))
+#    print("score_guess(expand_color_string('yauga'),expand_color_string('yauar'))")
+#    print(score_guess(expand_color_string('yauga'),expand_color_string('yauar')))
+#    sample_poss = [ALL_GUESSES[i] for i in range(100)]#[0,1,2,3,4,5,6,1000,1100]]
+#    print("sample_poss")
+#    print(sample_poss)
+#    sample_history = [
+#        (('black', 'black', 'black', 'black', 'black'),(2,0)),
+#        (('pink', 'pink', 'pink', 'pink', 'pink'),(0,0)),
+#    ]
+#    sample_history = [
+#        (('a', 'a', 'u', 'g', 'a'),(3,0)),
+#    ]
+#    print("sample_history")
+#    print(sample_history)
+#    print("valid_possibility(sample_poss[0], sample_history)")
+#    print(valid_possibility(sample_poss[0], sample_history))
+#    print("update_possibilities(sample_poss, sample_history)")
+#    print(update_possibilities(sample_poss, sample_history))
+#    sys.exit()
 
-
-
+    current_poss = ALL_GUESSES#[0:100]
+    current_history = []
+    while True:
+        expectations = [0] * len(current_poss)
+        printProgressBar(0,len(current_poss), length = 50)
+        for i, ith_guess in enumerate(current_poss):
+            printProgressBar(i,len(current_poss), length = 50)
+            countlist = [0] * len(current_poss)
+            for j, jth_guess in enumerate(current_poss):
+                jth_history = current_history + [(jth_guess, score_guess(ith_guess, jth_guess))]
+                reduced_poss = update_possibilities(current_poss, jth_history)
+                countlist[j] = len(current_poss) - len(reduced_poss)
+            expectations[i] = sum(countlist) / len(countlist)
+        maxval = max(expectations)
+        maxind = [i for i,x in enumerate(expectations) if x == maxval][0]
+        print('My guess is:')
+        print([COLOR_DICT[col] for col in current_poss[maxind]])
+        numred = int(input('Num reds?'))
+        if numred == NUM_SPOTS:
+            print("Done! Good game!")
+            sys.exit()
+        numwhite = int(input('Num whites?'))
+        current_history.append((current_poss[maxind], (numred,numwhite)))
+        current_poss = update_possibilities(current_poss, current_history)
+        if len(current_poss) < 1:
+            print("No possibilities remaining :(")
+            sys.exit()
 
 
