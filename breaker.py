342b732e2a559783bfa6a2ad86d04e921ff58a8c
Alexander Foss <alexanderhfoss@gmail.com>
2019-02-18 22:04:35 -0700

    Add numpy

--- breaker.py
+++ breaker.py
@@ -1,21 +1,24 @@
 import sys
+import time
 import itertools as it
+import numpy as np
 from os import system
 from random import choices
 from random import sample
 
 # TODO: Allow guesses not in set of possibilities
-# TODO: Numpy version with 0-7 representing colors
 # TODO: Speedup main routine: cython?
 # TODO: Monte Carlo sampling of options until specified time length elapses
+# TODO: Multicore
 
 NUM_SPOTS = 3
+MAX_GUESS_TIME = 60 # in seconds
 
 COLOR_LIST = [ 'black', 'blue', 'green', 'orange', 'pink', 'purple', 'white', 'yellow', ]
-COLOR_ABBREVS = ['a','u','g','o','i','r','w','y']
+COLOR_ABBREVS = list(range(len(COLOR_LIST))) #['a','u','g','o','i','r','w','y']
 COLOR_DICT = dict(zip(COLOR_ABBREVS, COLOR_LIST))
 
-ALL_GUESSES = list(it.product(*[COLOR_ABBREVS]*NUM_SPOTS))
+ALL_GUESSES = np.array(np.meshgrid(*[COLOR_ABBREVS]*NUM_SPOTS), dtype = np.uint8).T.reshape(-1,NUM_SPOTS)
 
 def playNote (duration, freq):
     system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
@@ -71,14 +74,14 @@ def valid_possibility(possibility, history):
     possibility: a color 5-tuple (adjust 5 based on NUM_SPOTS)
     history: a list of (color 5-tuple, (r,w)) tuples
     """
-    return all([hh[1] == score_guess(guess = hh[0], truth = possibility) for hh in history])
+    return history[1] == score_guess(guess = history[0], truth = possibility)
 
 def update_possibilities(possibilities, history):
     """
     possibilities: a list of color 5-tuples
     history: a list of (color 5-tuple, (r,w)) tuples
     """
-    return [pp for pp in possibilities if valid_possibility(pp, history)]
+    return np.array([pp for pp in possibilities if valid_possibility(pp, history)], dtype = np.uint8)
 
 if __name__ == "__main__":
     current_poss = ALL_GUESSES#[0:100]
@@ -87,29 +90,31 @@ if __name__ == "__main__":
     while True:
         if first_guess:
             # with replacement
-            current_guess = choices(COLOR_ABBREVS, k = NUM_SPOTS)
+            current_guess = np.array(choices(COLOR_ABBREVS, k = NUM_SPOTS), dtype = np.uint8)
             # without replacement
-            current_guess = sample(COLOR_ABBREVS, NUM_SPOTS)
+            current_guess = np.array(sample(COLOR_ABBREVS, NUM_SPOTS), dtype = np.uint8)
+            current_guess = np.array([0] * NUM_SPOTS, dtype = np.uint8)
             first_guess = False
         else:
-            num_choices_remaining = len(current_poss)
+            num_choices_remaining = current_poss.shape[0]
             expectations = [0] * num_choices_remaining
             print('Number of possibilities remaining: ', str(num_choices_remaining))
             printProgressBar(0,num_choices_remaining, length = 50)
-            for i, ith_guess in enumerate(current_poss):
+            start = time.time()
+            for i in range(num_choices_remaining):
                 printProgressBar(i,num_choices_remaining, length = 50)
                 countlist = [0] * num_choices_remaining
-                for j, jth_guess in enumerate(current_poss):
-                    new_history = [(jth_guess, score_guess(ith_guess, jth_guess))]
-                    jth_history = current_history + new_history
+                for j in range(num_choices_remaining):
+                    new_history = (current_poss[j,:], score_guess(current_poss[i,:], current_poss[j,:]))
                     reduced_poss = update_possibilities(current_poss, new_history)
-                    countlist[j] = len(reduced_poss)
+                    countlist[j] = reduced_poss.shape[0]
                 expectations[i] = sum(countlist)
             printProgressBar(num_choices_remaining,num_choices_remaining, length = 50)
             minval = min(expectations)
             minind = [i for i,x in enumerate(expectations) if x == minval][0]
-            current_guess = current_poss[minind]
+            current_guess = current_poss[minind,:]
             playNote(1/4, 3/2 * 440)
+            print(str(round(time.time() - start, 1)) + ' seconds elapsed.')
         print('My guess is:')
         print([COLOR_DICT[col] for col in current_guess])
         numred = int(input('Num reds?  '))
@@ -117,9 +122,8 @@ if __name__ == "__main__":
             print("Done! Good game!")
             sys.exit()
         numwhite = int(input('Num whites?  '))
-        current_history.append((current_guess, (numred,numwhite)))
-        current_poss = update_possibilities(current_poss, current_history)
-        if len(current_poss) < 1:
+        current_poss = update_possibilities(current_poss, (current_guess, (numred, numwhite)))
+        if current_poss.shape[0] < 1:
             print("No possibilities remaining :(")
             sys.exit()
 
